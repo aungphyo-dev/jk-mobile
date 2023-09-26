9437bea2d2a7b1b058ab2816bb2d78a757d4ac0d
aungpyaephyo1412 <aungpyaephyo1412@gmail.com>
2023-09-26 18:39:36 +0630

    loading fix

--- src/layouts/Detail/Detail.jsx
+++ src/layouts/Detail/Detail.jsx
@@ -11,6 +11,7 @@ import Button from '@mui/material/Button';
 import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import RemoveShoppingCartIcon from '@mui/icons-material/RemoveShoppingCart';
 import Shop2Icon from '@mui/icons-material/Shop2';
+import Skeleton from "@mui/material/Skeleton";
 const Detail = () => {
     const {id} = useParams()
     const dispatch = useDispatch()
@@ -44,6 +45,15 @@ const Detail = () => {
     }, [product]);
     return (
         <section className='max-w-screen-xl mx-auto px-3 min-h-screen'>
+            {isLoading && <div className="space-y-2">
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+                <Skeleton variant="rectangular" height={60} />
+            </div>}
             {!isLoading && <div className='grid grid-cols-7 gap-5'>
                 <div className='col-span-7 md:col-span-3 lg:col-span-2 bg-white rounded-xl px-4 py-6'>
                     <img className='h-[245px] object-contain mx-auto'
--- src/layouts/Loading/Loading.jsx
+++ src/layouts/Loading/Loading.jsx
@@ -1,76 +1,71 @@
 import Skeleton from '@mui/material/Skeleton';
+import Stack from '@mui/material/Stack';
+
 const Loading = () => {
     return (
         <div className='w-full h-auto grid grid-cols-2 md:grid-cols-4 lg:grid-cols-7 gap-x-4'>
             <div className='w-full'>
-                <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full hidden md:block'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full hidden md:block'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full hidden lg:block'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full hidden lg:block'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
             <div className='w-full hidden lg:block'>
-                 <Skeleton />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation="wave" />
-                <Skeleton animation={false} />
+                <Stack spacing={1}>
+                    {/* For variant="text", adjust the height via font-size */}
+                    <Skeleton variant="text" sx={{ fontSize: '1rem' }} />
+                    {/* For other variants, adjust the size with `width` and `height` */}
+                    <Skeleton variant="rectangular" height={60} />
+                    <Skeleton variant="rounded" height={60} />
+                </Stack>
             </div>
 
         </div>
